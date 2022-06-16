7af55a2d976f50e0f731d8835abbe34d0f6ec0b7
sinnan-security <sinnan.suleman@daraz.pk>
2022-06-16 14:48:39 +0500

    Implement other functionality

--- app.py
+++ app.py
@@ -46,6 +46,12 @@ def SomeFunctionality():
 	response={}
 	logger(request,response)
 	return "<h1>notification service %s</H1>"%(rand)
+	
+@app.route('/api/notification/OtherRoute', methods=['POST'])
+def Otherfunctionality():
+	response={}
+	logger(request,response)
+	return "<h1>Some Response!</H1>"
 
 def logger(request,response):
 	tmp='headers:{'
